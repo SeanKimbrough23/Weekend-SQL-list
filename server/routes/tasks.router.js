c59a71b15c6252e37854a40a4f353752dfd6a94f
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-03-11 11:25:59 -0600

    Delete request is working, working in POSTMAN as well, now switching to PUT

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool.js');
 
 let tasks = [];
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { 
     const queryText = 'SELECT * FROM "to-do";';
     console.log('Submitting Query to DB' , queryText);
     
@@ -40,22 +40,43 @@ router.post('/', (req, res) => {
    // tasks.push(req.body);
    //res.sendStatus(200);
 });
+// DELETE song from database using ID
+router.delete('/:id', (req, res)=> {
+    console.log('Inside of tasks/:id, DELETE request');
+    const idToDelete = [req.params.id];
+    console.log('id to delete', idToDelete);
+    const queryText = `DELETE FROM "to-do" WHERE id=$1`
 
+    pool.query(queryText, [Number(idToDelete)])// has to be in an array everytime 
+    .then ((results) => {
+        console.log('Successful Deletion for task id:', idToDelete);
+        res.sendStatus(200);
+    })
+    .catch ((error) => {
+        console.log(`Error making query: ${queryText}` , error);
+        res.sendStatus(500)
+    })
+})
 
+//UPDATE  for a tasks to make it 1 using ID
+router.put('/:id', (req, res) => {
+    console.log('Tasks to update rank by id:' , idToPut);
 
+    const idToPut = [req.params.id];
+    const queryText = `UPDATE tasks SET "id" = 1
+    WHERE id = $1`
 
-
-
-
-
-
-
-
-
-
-
-
-
+    pool.query(queryText, [idToPut])
+    .then ((results) => {
+        console.log('update successful for idToPut:' , idToPut);
+        res.sendStatus(200); 
+    }) .catch((error) => {
+        console.log('Error making update to tasks for idToPut:', idToPut);
+        console.log("update queryText:", queryText);
+        res.sendStatus(500);
+    })
+     
+})
 
 
 
