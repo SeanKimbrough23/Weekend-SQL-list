e315d9668f7f4b8e27348d95bbee190f32e4461e
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-03-11 12:26:54 -0600

    PUT & DELETE method complete, POSTMAN working and update SQL database  now time to work on client side

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -41,13 +41,13 @@ router.post('/', (req, res) => {
    //res.sendStatus(200);
 });
 // DELETE song from database using ID
-router.delete('/:id', (req, res)=> {
+router.delete('/:id', (req, res)=> { //make sure route is not /tasks/:id, we only we use /:id because we are only using the tasks route
     console.log('Inside of tasks/:id, DELETE request');
-    const idToDelete = [req.params.id];
+    const idToDelete = [req.params.id]; 
     console.log('id to delete', idToDelete);
-    const queryText = `DELETE FROM "to-do" WHERE id=$1`
+    const queryText = `DELETE FROM "to-do" WHERE id=$1` // make sure DELETE FROM " " matches the table name in SQL
 
-    pool.query(queryText, [Number(idToDelete)])// has to be in an array everytime 
+    pool.query(queryText, [Number(idToDelete)])// has to be in an array everytime & since it is expecting a number, it was sending as a string so we need 
     .then ((results) => {
         console.log('Successful Deletion for task id:', idToDelete);
         res.sendStatus(200);
@@ -60,23 +60,33 @@ router.delete('/:id', (req, res)=> {
 
 //UPDATE  for a tasks to make it 1 using ID
 router.put('/:id', (req, res) => {
-    console.log('Tasks to update rank by id:' , idToPut);
-
-    const idToPut = [req.params.id];
-    const queryText = `UPDATE tasks SET "id" = 1
-    WHERE id = $1`
-
-    pool.query(queryText, [idToPut])
-    .then ((results) => {
-        console.log('update successful for idToPut:' , idToPut);
-        res.sendStatus(200); 
-    }) .catch((error) => {
-        console.log('Error making update to tasks for idToPut:', idToPut);
-        console.log("update queryText:", queryText);
+    console.log('/  PUT;', req.params.id, req.body);
+    const queryText = `UPDATE "to-do" SET complete=$1 WHERE id=$2;`;
+    const values = [req.body.newComplete, req.params.id];// params is 2nd thing in array why we need $2
+    pool.query(queryText, values).then((results)=> {
+        res.sendStatus(200);
+    }).catch((error)=> {
+        console.log('error with update:', error);
         res.sendStatus(500);
     })
-     
 })
+
+//     const idToPut = [req.params.id];
+//     console.log('Tasks to update tasks by id:' , idToPut);
+//     const queryText = `UPDATE tasks SET "id" = 1
+//     WHERE id = $1`
+
+//     pool.query(queryText, [idToPut])
+//     .then ((results) => {
+//         console.log('update successful for idToPut:' , idToPut);
+//         res.sendStatus(200); 
+//     }) .catch((error) => {
+//         console.log('Error making update to tasks for idToPut:', idToPut);
+//         console.log("update queryText:", queryText);
+//         res.sendStatus(500);
+//     })
+     
+// })
 
 
 
