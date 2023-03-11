47e5b700ede9ac0c5832d82bac770ae04869d8c4
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-03-11 16:10:31 -0600

    getting client side to work! GET working so far

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,102 @@
 $(document).ready(onReady);
 
-function onReady (){
-    console.log('inside onReady function');
+function onReady() {
+  console.log("inside onReady function");
+  //event listeners
+  $("#submitButton").on("click", submitTasks);
+  //$('#tasksOut').on('click', '.submitButton', putTasks);
+  getTasks();
+}
+
+function submitTasks() {
+  let objectToSend = {
+    day: $("#date").val(),
+    task: $("#task").val(),
+    complete: $("#complete").val(),
+  };
+  console.log("Inside submitTasks:", objectToSend);
+  $.ajax({
+    type: "POST",
+    url: "/tasks",
+    data: objectToSend,
+  })
+    .then(function (response) {
+      console.log("response back from POST:", response);
+      $("input").val("");
+      getTasks();
+    })
+    .catch(function (error) {
+      alert("error adding tasks:", error);
+    });
+}
+//  function getTasks() {
+//     $("#tasksOut").empty();
+//     $.ajax({
+//         type: 'GET',
+//         url: '/tasks'
+//     }).then(function (response) {
+//         console.log("GET /tasks response", response);
+//         // append data to the DOM
+//         for (let i = 0; i < tasks.length; i++) {
+//             $('#tasksOut').append(`
+//                 <tr data-id=${tasks[i].id}>
+//                     <td>${tasks[i].date}</td>
+//                     <td>${tasks[i].task}</td>
+//                     <td>${tasks[i].complete}</td>
+//                     <td><button class="deletebtn"> delete</button> </td>
+//                     <td><button class="updateRankBtn"> Update Rank </button> </td>
+
+//                 </tr>
+//             `);
+//         }
+//     }
+//  }
+
+function getTasks() {
+  $.ajax({
+    type: "GET",
+    url: "/tasks",
+  })
+    .then(function (response) {
+      console.log(response);
+      render(response);
+    })
+    .catch(function (error) {
+      alert("error getting Tasks:", error);
+    });
+}
+
+function render(tasks) {
+  console.log(tasks);
+  let el = $("#tasksOut");
+  el.empty();
+  for (let i = 0; i < tasks.length; i++) {
+    $("#tasksOut").append(`
+        <tr data-id=${tasks[i].id}>
+            <td>${tasks[i].task}</td>
+            <td>${tasks[i].complete}</td>
+            <td><button class="deletebtn"> delete</button> </td>
+            <td><button class="completeBtn">complete</button> </td>
+
+        </tr>
+    `);
+  }
+}
+
+function deleteTasks() {
+  const id = $(this).data("id");
+  const deleteThis = $(this).data("complete");
+  console.log("inside deleteTasks:", id, deleteThis);
+  $.ajax({
+    type: "PUT",
+    url: `/tasks/${id}`,
+    data: { newTasks: deleteThis },
+  })
+    .then(function (response) {
+      console.log("response back from PUT request:", response);
+      getTasks();
+    })
+    .catch(function (error) {
+      alert("error updating:", error);
+    });
 }
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -1,75 +1,80 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const pool = require("../modules/pool.js");
 
 let tasks = [];
 
-router.get('/', (req, res) => { 
-    const queryText = 'SELECT * FROM "to-do";';
-    console.log('Submitting Query to DB' , queryText);
-    
-    pool.query(queryText)
-    .then ((result) => { 
-        console.log("All my Result info:" , result);
-        res.send(result.rows);
-    })
-    .catch((error) => {
-        console.log(`Error making query ${queryText}`,error)
-        res.sendStatus(500);
-    });
+router.get("/", (req, res) => {
+  const queryText = 'SELECT * FROM "to-do";';
+  console.log("Submitting Query to DB", queryText);
 
-
-});
-router.post('/', (req, res) => {
-    const newTask= req.body
-    console.log('My new tasks', newTask);
-    console.log(newTask);
-
-    const queryText = `INSERT INTO "to-do" ("day","task","complete")
-    VALUES ($1, $2, $3); 
-    `; //need to start at $1 and go up to as many values as you have 
-
-    pool.query(queryText, [newTask.day, newTask.tasks, newTask.complete])
+  pool
+    .query(queryText)
     .then((result) => {
-        res.sendStatus(201)
+      console.log("All my Result info:", result);
+      res.send(result.rows);
     })
     .catch((error) => {
-        console.log(`Error making query ${queryText}`, error);
-        res.sendStatus(500);
+      console.log(`Error making query ${queryText}`, error);
+      res.sendStatus(500);
+    });
+});
+router.post("/", (req, res) => {
+  const newTask = req.body;
+  console.log("My new tasks", newTask);
+  console.log(newTask);
+
+  const queryText = `INSERT INTO "to-do" ("day","task","complete")
+    VALUES ($1, $2, $3); 
+    `; //need to start at $1 and go up to as many values as you have
+
+  pool
+    .query(queryText, [newTask.day, newTask.task, newTask.complete])
+    .then((result) => {
+      res.sendStatus(201);
     })
-   // tasks.push(req.body);
-   //res.sendStatus(200);
+    .catch((error) => {
+      console.log(`Error making query ${queryText}`, error);
+      res.sendStatus(500);
+    });
+  // tasks.push(req.body);
+  //res.sendStatus(200);
 });
 // DELETE song from database using ID
-router.delete('/:id', (req, res)=> { //make sure route is not /tasks/:id, we only we use /:id because we are only using the tasks route
-    console.log('Inside of tasks/:id, DELETE request');
-    const idToDelete = [req.params.id]; 
-    console.log('id to delete', idToDelete);
-    const queryText = `DELETE FROM "to-do" WHERE id=$1` // make sure DELETE FROM " " matches the table name in SQL
+router.delete("/:id", (req, res) => {
+  //make sure route is not /tasks/:id, we only we use /:id because we are only using the tasks route
+  console.log("Inside of tasks/:id, DELETE request");
+  const idToDelete = [req.params.id];
+  console.log("id to delete", idToDelete);
+  const queryText = `DELETE FROM "to-do" WHERE id=$1`; // make sure DELETE FROM " " matches the table name in SQL
 
-    pool.query(queryText, [Number(idToDelete)])// has to be in an array everytime & since it is expecting a number, it was sending as a string so we need 
-    .then ((results) => {
-        console.log('Successful Deletion for task id:', idToDelete);
-        res.sendStatus(200);
+  pool
+    .query(queryText, [Number(idToDelete)]) // has to be in an array everytime & since it is expecting a number, it was sending as a string so we need
+    .then((results) => {
+      console.log("Successful Deletion for task id:", idToDelete);
+      res.sendStatus(200);
     })
-    .catch ((error) => {
-        console.log(`Error making query: ${queryText}` , error);
-        res.sendStatus(500)
-    })
-})
+    .catch((error) => {
+      console.log(`Error making query: ${queryText}`, error);
+      res.sendStatus(500);
+    });
+});
 
 //UPDATE  for a tasks to make it 1 using ID
-router.put('/:id', (req, res) => {
-    console.log('/  PUT;', req.params.id, req.body);
-    const queryText = `UPDATE "to-do" SET complete=$1 WHERE id=$2;`;
-    const values = [req.body.newComplete, req.params.id];// params is 2nd thing in array why we need $2
-    pool.query(queryText, values).then((results)=> {
-        res.sendStatus(200);
-    }).catch((error)=> {
-        console.log('error with update:', error);
-        res.sendStatus(500);
+router.put("/:id", (req, res) => {
+  console.log("/  PUT;", req.params.id, req.body);
+  const queryText = `UPDATE "to-do" SET complete=$1 WHERE id=$2;`;
+  const values = [req.body.newComplete, req.params.id]; // params is 2nd thing in array why we need $2
+  pool
+    .query(queryText, values)
+    .then((results) => {
+      res.sendStatus(200);
     })
-})
+    .catch((error) => {
+      console.log("error with update:", error);
+      res.sendStatus(500);
+    });
+});
 
 //     const idToPut = [req.params.id];
 //     console.log('Tasks to update tasks by id:' , idToPut);
@@ -79,15 +84,13 @@ router.put('/:id', (req, res) => {
 //     pool.query(queryText, [idToPut])
 //     .then ((results) => {
 //         console.log('update successful for idToPut:' , idToPut);
-//         res.sendStatus(200); 
+//         res.sendStatus(200);
 //     }) .catch((error) => {
 //         console.log('Error making update to tasks for idToPut:', idToPut);
 //         console.log("update queryText:", queryText);
 //         res.sendStatus(500);
 //     })
-     
+
 // })
-
-
 
 module.exports = router;
